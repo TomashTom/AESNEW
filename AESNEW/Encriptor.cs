bf3d529df230cdceac828af7198cb1ca5ba8d599
TomashTom <smolskijt@gmail.com>
2022-03-11 19:07:16 +0200

    Add project files.

---
+++ AESNEW/Encriptor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace AESNEW
+{
+    class Encriptor
+    {
+
+        public static string IV = "1a1a1a1a1a1a1a1a";
+        public static string Key = "1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a13";
+
+        public static string Encript(string decriped)
+        {
+            byte[] textByte = ASCIIEncoding.ASCII.GetBytes(decriped);
+            AesCryptoServiceProvider endec = new AesCryptoServiceProvider();
+            endec.BlockSize = 128;
+            endec.KeySize = 256;
+            endec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            endec.Key = ASCIIEncoding.ASCII.GetBytes(Key);
+            endec.Padding = PaddingMode.PKCS7;
+            endec.Mode = CipherMode.CBC;
+            ICryptoTransform icrypt = endec.CreateEncryptor(endec.Key, endec.IV);
+            byte[] enc = icrypt.TransformFinalBlock(textByte, 0, textByte.Length);
+            icrypt.Dispose();
+            return Convert.ToBase64String(enc);
+            
+
+
+        }
+
+        public static string Decrypte(string encrypted)
+        {
+
+            var check = Form1.verify;
+            string password = Form1.password;
+            if (check == password)
+            {
+                
+                byte[] textBytes = Convert.FromBase64String(encrypted);
+                AesCryptoServiceProvider endec = new AesCryptoServiceProvider();
+                endec.BlockSize = 128;
+                endec.KeySize = 256;
+                endec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+                endec.Key = ASCIIEncoding.ASCII.GetBytes(Key);
+                endec.Mode = CipherMode.CBC;
+                ICryptoTransform icrypt = endec.CreateDecryptor(endec.Key, endec.IV);
+                byte[] enc = icrypt.TransformFinalBlock(textBytes, 0, textBytes.Length);
+                icrypt.Dispose();
+            }
+
+            /*if (Form1.verify == password)
+            {
+                return System.Text.ASCIIEncoding.ASCII.GetString(enc);
+            }
+            else if (Form1.verify != password)
+            {
+                MessageBox.Show("Use originas key file");
+            }*/
+
+            return encrypted;
+        }
+        
+
+    }
+}
---
+++ AESNEW/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AESNEW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string password;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            password = textBox1.Text;
+            string encm = Encriptor.Encript(richTextBox1.Text);
+            System.IO.File.WriteAllText(@"C:\Demo\private.txt", encm);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"C:\Demo\private.txt");
+            string line = sr.ReadLine();
+
+            richTextBox1.Text = Encriptor.Decrypte(Convert.ToString(line));
+
+        }
+        public static string verify;
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            verify = textBox2.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox1.Text;
+        }
+    }
+}
